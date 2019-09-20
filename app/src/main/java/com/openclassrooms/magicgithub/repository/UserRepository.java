05cb709597068ce150ae0b256a08a37f8a3d333d
docteurmabuse <laurent.tizzone@gmail.com>
2019-09-20 16:22:26 +0200

    - Add return value at getUsers method in UserRepository using apiService - Add return value at getUsers method in FakeApiService

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -7,15 +7,14 @@ import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
 
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+        return apiService.getUsers();
     }
 
     public void generateRandomUser() {
